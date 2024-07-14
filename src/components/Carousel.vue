1060aa4f033457f71028959e66b811879ba08a31
chien pei <com2100927@gmail.com>
2024-07-15 02:58:16 +0800

    Carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -123,18 +123,107 @@ export default {
     <div class="container">
         <div class="slider">
             <div class="owl-carousel">
-                <div class="slider-card" v-for="(event, index) in events" :key="index">
-                    <img :src="event.image" alt="Event image">
+                <div class="slider-card">
+                    <img src="./assets/photo/NS陽金.jpg" alt="NS陽金">
                     <div class="cardBody">
-                        <h4>{{ event.title }}</h4>
-                        <p>{{ event.date }}</p>
-                        <p>{{ event.route }}</p>
+                        <h4>1月 NeverStop ：陽金P字山道挑戰</h4>
+                        <p>日期：2024年1月7日（星期日）</p>
+                        <p>活動路線：總爬升約2,200公尺;全程約75公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/TWB東三塔.jpg" alt="TWB東三塔">
+                    <div class="cardBody">
+                        <h4>2月 TWB 東三塔550</h4>
+                        <p>日期：2024年2月24日（星期六） ～ 2024年2月25日（星期日）</p>
+                        <p>活動路線：總爬升約5,000公尺;全程約550公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/KK120.jpg" alt="KK120">
+                    <div class="cardBody">
+                        <h4>3月 GIANT KK120 自我挑戰活動</h4>
+                        <p>日期：2024年3月17日（星期日）</p>
+                        <p>活動路線：總爬升約300公尺;全程約95公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/環花東365.jpg" alt="環花東365">
+                    <div class="cardBody">
+                        <h4>4月 環花東365自行車挑戰</h4>
+                        <p>日期：2024年04月13日（星期六）～ 2024年04月14日（星期日）</p>
+                        <p>活動路線：總爬升約2,000公尺;全程約340公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/雙溪Liv day.jpg" alt="雙溪Liv day">
+                    <div class="cardBody">
+                        <h4>5月 LIVDAY初夏大山-騎訪雙溪</h4>
+                        <p>日期：2024年05月26日（星期日）</p>
+                        <p>活動路線：總爬升約1,164公尺;全程約66公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/紫南宮.jpg" alt="紫南宮">
+                    <div class="cardBody">
+                        <h4>6月 NeverStop：一騎來發財紫南宮環縣極限挑戰</h4>
+                        <p>日期：2024年06月02日（星期日）</p>
+                        <p>活動路線：總爬升約800公尺;全程約99.9公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/武嶺.jpg" alt="武嶺">
+                    <div class="cardBody">
+                        <h4>7月 TIS 崇越盃武嶺自行車挑戰賽</h4>
+                        <p>日期：2024年06月24日（星期一）</p>
+                        <p>活動路線：總爬升約2,623公尺;全程約52公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/環大苗栗.jpg" alt="環大苗栗">
+                    <div class="cardBody">
+                        <h4>8月 TBA 環大苗栗經典挑戰</h4>
+                        <p>日期：2024年08月25日（星期日）</p>
+                        <p>活動路線：總爬升約1,200公尺;全程約107公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/雪見司馬.jpg" alt="雪見司馬">
+                    <div class="cardBody">
+                        <h4>9月 GIANT CUP 百里千尺 - 雪見司馬</h4>
+                        <p>日期：2024年09月01日（星期日）</p>
+                        <p>活動路線：總爬升約2,400公尺;全程約136公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/時代騎輪節.jpg" alt="時代騎輪節">
+                    <div class="cardBody">
+                        <h4>10月 時代騎輪節</h4>
+                        <p>日期：2024年10月20日（星期日）</p>
+                        <p>活動路線：總爬升約600公尺;全程約102公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/TBA北高360.jpg" alt="TBA北高360">
+                    <div class="cardBody">
+                        <h4>11月 TBA 北高360</h4>
+                        <p>日期：2024年11月08日（星期五）</p>
+                        <p>活動路線：總爬升約300公尺;全程約360公里</p>
+                    </div>
+                </div>
+                <div class="slider-card">
+                    <img src="./assets/photo/戀戀197.jpg" alt="戀戀197">
+                    <div class="cardBody">
+                        <h4>12月 戀戀197</h4>
+                        <p>日期：2024年12月01日（星期日）</p>
+                        <p>活動路線：總爬升約1,100公尺;全程約130公里</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
 
 <style scoped lang="scss">
 * {
